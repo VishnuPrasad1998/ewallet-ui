be3903a6297ac7021d66cf67539e8a4d1f3648cd
Vishnu Prasad <vishnu1998prasad@gmail.com>
2021-12-03 15:25:13 +0530

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
     htmlAttrs: {
       lang: 'en'
     },
-    target: 'static',
+    target: 'serverless',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
